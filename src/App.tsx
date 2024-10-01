c133dbe86494ac150b75002e6846473051787fb3
Isak Pettersson <isak.pettersson@apoteket.se>
2024-10-01 14:48:25 +0200

    add scanner

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { QrReader } from './Components/QrScanner';
 
 function App() {
   return (
@@ -8,16 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reloa.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <input title='Enter here' id="my-input"/>
+        <QrReader />
       </header>
     </div>
   );
